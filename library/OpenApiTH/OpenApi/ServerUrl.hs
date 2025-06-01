e9ccb7f18d05ed2a780e730a0ddff623bcf93607
Chris Martin <ch.martin@gmail.com>
2025-06-01 01:30:25 -0600

    readServerUrl

--- library/OpenApiTH/OpenApi/ServerUrl.hs
+++ library/OpenApiTH/OpenApi/ServerUrl.hs
@@ -10,6 +10,7 @@ import Control.Monad.Validate
 import Data.ByteString (ByteString)
 import Data.Either (Either (..), either)
 import Data.String (IsString)
+import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Word
 import Iri.Data
@@ -25,17 +26,21 @@ data ServerUrl = ServerUrl
   }
   deriving stock (Lift, Eq, Show)
 
+readServerUrl ∷ Text → Either [Text] ServerUrl
+readServerUrl t = case P.httpIri t of
+  Left e → Left [e]
+  Right (HttpIri security host port path query fragment) → runValidate do
+    unless (query == Query "") $ dispute ["Query must be empty"]
+    unless (fragment == Fragment "") $ dispute ["Fragment must be empty"]
+    pure ServerUrl {security, host, port, path}
+
 serverUrlQQ ∷ QuasiQuoter
 serverUrlQQ =
   QuasiQuoter
-    { quoteExp = \s → case P.httpIri (Text.pack s) of
-        Left e → fail $ Text.unpack e
-        Right (HttpIri security host port path query fragment) → do
-          result ← runValidateT do
-            unless (query == Query "") $ dispute ["Query must be empty"]
-            unless (fragment == Fragment "") $ dispute ["Fragment must be empty"]
-            pure ServerUrl {security, host, port, path}
-          either (fail . Text.unpack . Text.intercalate "\n") lift result
+    { quoteExp =
+        either (fail . Text.unpack . Text.intercalate "\n") lift
+          . readServerUrl
+          . Text.pack
     }
 
 localhost ∷ ServerUrl
