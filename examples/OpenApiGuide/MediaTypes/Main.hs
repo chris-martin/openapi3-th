c64ff895064f10d73ede1a071506f70e4cbd84ad
Chris Martin <ch.martin@gmail.com>
2025-06-20 01:28:30 -0600

    failing spec

---
+++ examples/OpenApiGuide/MediaTypes/Main.hs
@@ -0,0 +1,65 @@
+module OpenApiGuide.MediaTypes.Main where
+
+import Essentials
+
+import Test.Hspec
+
+import Data.ByteString.Builder qualified as BSB
+import Data.Foldable
+import Data.List (map)
+import Data.Text (Text)
+import Language.Haskell.TH qualified as TH
+import List.Transformer qualified as ListT
+import Network.HTTP.Simple
+import Network.Wai.Handler.Warp
+import System.IO (IO)
+import Prelude (fromIntegral, show)
+
+import OpenApiTH
+
+declare $
+  specFile "examples/OpenApiGuide/MediaTypes/openapi1.yaml"
+    <> operation ("get /employees" & setOperationName "GetEmployees")
+
+server ∷ OperationServer GetEmployees IO
+server () =
+  pure
+    [ Employee {id = 5, name = "Klaus", fullTime = True}
+    , Employee {id = 12, name = "Niko", fullTime = False}
+    ]
+
+spec ∷ Spec
+spec = describe "" do
+  it @Expectation "round trip" do
+    assertHttpClientWarpExchange @GetEmployees () server
+
+  it @Expectation "request" do
+    request ← buildOperationRequest @GetEmployees ()
+    request.head
+      `shouldBe` OutgoingRequest
+        { location =
+            ResourceLocation
+              { scheme = Nothing
+              , context = AbsoluteContext
+              , path = ["employees"]
+              }
+        , method = "GET"
+        , query = []
+        , accept = "application/json"
+        }
+    requestBody ← ListT.fold (<>) mempty id request.body
+    BSB.toLazyByteString requestBody `shouldBe` ""
+
+  it @Expectation "response" do
+    response ←
+      buildOperationResponse @GetEmployees
+        [ Employee {id = 5, name = "Klaus", fullTime = True}
+        , Employee {id = 12, name = "Niko", fullTime = False}
+        ]
+    response.head
+      `shouldBe` OutgoingResponse
+        { statusCode = "200"
+        , contentType = "application/json"
+        }
+    responseBody ← ListT.fold (<>) mempty id response.body
+    BSB.toLazyByteString responseBody `shouldBe` "..."
