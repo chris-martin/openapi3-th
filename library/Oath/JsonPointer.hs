16115c9ba7e5e53dafea13132f78c64d46a7c765
Chris Martin <ch.martin@gmail.com>
2025-08-02 01:19:38 -0600

    sketch out json pointer

--- library/Oath/JsonPointer.hs
+++ library/Oath/JsonPointer.hs
@@ -7,22 +7,45 @@ import Data.Aeson qualified as JSON
 import Data.Aeson.Key qualified as Key
 import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Aeson.Types
+import Data.Char (Char)
 import Data.Int (Int)
 import Data.Sequence (Seq (..))
 import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Vector qualified as V
+import Optics hiding (Empty)
 
-newtype JsonPointer = JsonPointer (Seq Text)
+import Oath.Grammar
+import Numeric.Natural (Natural)
 
--- jsonPointerGrammar
+jsonPointerGrammar ∷ Grammar Text (Seq Text)
+jsonPointerGrammar =
+  label "json-pointer" $
+    seqGrammar $
+      constGrammar "/" +> referenceTokenGrammar
+
+referenceTokenGrammar ∷ Grammar Text Text
+referenceTokenGrammar = label "reference-token" $
+  isoGrammar (iso T.unpack T.pack) $
+    listGrammar $
+      grammarAlternatives
+        [ unescapedGrammar
+        , escapedGrammar
+        ]
+
+unescapedGrammar ∷ Grammar Text Char
+unescapedGrammar = label "unescaped" _
+
+escapedGrammar ∷ Grammar Text Char
+escapedGrammar = label "escaped" _
 
 -- | https://www.rfc-editor.org/rfc/rfc6901#section-4
-evaluateJsonPointer ∷ JsonPointer → Value → Maybe Value
-evaluateJsonPointer =
-  \(JsonPointer items) → go items
+evaluateJsonPointer ∷ Seq Text → Value → Maybe Value
+evaluateJsonPointer = go
  where
-  go Empty = Just
-  go (x :<| xs) = go xs <=< evaluateJsonReferenceToken x
+  go = \case
+    Empty → Just
+    (x :<| xs) → go xs <=< evaluateJsonReferenceToken x
 
 evaluateJsonReferenceToken ∷ Text → Value → Maybe Value
 evaluateJsonReferenceToken x = \case
@@ -32,3 +55,6 @@ evaluateJsonReferenceToken x = \case
 
 tokenAsIndex ∷ Text → Maybe Int
 tokenAsIndex = _
+
+arrayIndexGrammar :: Grammar Text Natural
+arrayIndexGrammar = _
